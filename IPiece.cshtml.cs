bacdf4da89f8c691767a0d12dfaf4b95d356804c
Justin Yu <54873542+JustinPeipeiYu@users.noreply.github.com>
2023-05-13 22:37:42 -0400

    Add files via upload

---
+++ IPiece.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ChineseChessXiangQi.Pages;
+
+public interface IPiece
+{
+    protected string Name { get; set; }
+    protected static double Radius { get; set; }
+    protected double X { get; set; }
+    protected double Y { get; set; }
+    protected bool Display { get; set; }
+
+}
+
+
+
