181a57fd96bcee62b6d1d0d0a736a4ebd31e0c4c
Justin Yu <54873542+JustinPeipeiYu@users.noreply.github.com>
2023-05-16 13:42:57 -0400

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ChineseChessXiangQi.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public int X { get; set; } = 300 + 4 * 50;
+        public int Y { get; set; } = 100;
+        public void OnGet()
+        {
+
+        }
+    }
+}
+
+
