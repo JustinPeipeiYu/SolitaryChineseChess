22278ab7d47261001a53dc189be4261a0cf67ef6
Justin Yu <54873542+JustinPeipeiYu@users.noreply.github.com>
2023-05-19 12:32:28 -0400

    Add files via upload

---
+++ IPiece.cs
@@ -0,0 +1,14 @@
+﻿namespace ChineseChessXiangQi.Pages
+{
+    public interface IPiece
+    {
+        double X { get; }
+        double Y { get; }
+        string Name { get; }
+
+        string Color { get; }
+
+    }
+}
+
+
---
+++ Piece.cs
@@ -0,0 +1,21 @@
+﻿namespace ChineseChessXiangQi.Pages
+{
+    public class Piece: IPiece
+    {
+        public string Name { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public string Color { get; set; }
+
+        public Piece(string name, string color, double x, double y)
+        {
+            Color = color;
+            Name = name;
+            X = x;
+            Y = y;
+        }
+    }
+}
+
+
