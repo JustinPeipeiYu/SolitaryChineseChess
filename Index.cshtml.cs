19083502b32e0cbd1496632bee34590a70b3d11e
Justin Yu <54873542+JustinPeipeiYu@users.noreply.github.com>
2023-05-24 18:11:21 -0400

    Add files via upload

--- Index.cshtml.cs
+++ Index.cshtml.cs
@@ -22,26 +22,7 @@ namespace ChineseChessAlly.Pages
         public static double width = 60;
         public List<Position> positions = new List<Position>();
         public List<Piece> pieces = new List<Piece>();
-        public static int i = 0;
-        /*
-        public List<Piece> blackPieces = new List<Piece> { blackGeneral, blackGuard1, blackGuard2, blackElephant1, 
-            blackElephant2, blackHorse1, blackHorse2, blackRook1, blackRook2, blackCannon1, blackCannon2, 
-            blackSoldier1, blackSoldier2, blackSoldier3, blackSoldier4, blackSoldier5};
-        public List<Piece> redPieces = new List<Piece> { redGeneral, redGuard1, redGuard2, redElephant1, redElephant2, 
-            redHorse1, redHorse2, redRook1, redRook2, redCannon1, redCannon2, redSoldier1, redSoldier2, 
-            redSoldier3, redSoldier4, redSoldier5 };
-        */
-        /*
-        public bool checkInside(double x1, double y1, double x2, double y2, int width)
-        {
-            //takes any two coordinates and checks if the distance between is less than a given value
-            double a = x1 - x2;
-            double a2 = a * a;
-            double b = y1 - y2;
-            double b2 = b * b;
-            return (Math.Sqrt(a2 + b2) <= width);
-        }
-        */
+     
         
         public void OnGet()
         {
